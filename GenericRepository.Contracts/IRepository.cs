a37474b4edbc1ee896a1906980df9fe2bfff9497
Aidar <aiadr@pm.me>
2023-03-01 03:06:02 +0300

    add project

---
+++ GenericRepository.Contracts/IRepository.cs
@@ -0,0 +1,29 @@
+namespace GenericRepository.Contracts;
+
+public interface IRepository
+{
+    Task<TEntity?> GetAsync<TEntity>(
+        Func<IQueryable<TEntity>, IQueryable<TEntity>> queryBuilder,
+        CancellationToken cancellationToken = default)
+        where TEntity : class;
+
+    Task<List<TEntity>> GetListAsync<TEntity>(
+        Func<IQueryable<TEntity>, IQueryable<TEntity>> queryBuilder,
+        CancellationToken cancellationToken = default)
+        where TEntity : class;
+
+    Task<TResult?> GetReadOnlyAsync<TEntity, TResult>(
+        Func<IQueryable<TEntity>, IQueryable<TResult>> queryBuilder,
+        CancellationToken cancellationToken = default)
+        where TEntity : class;
+
+    IAsyncEnumerable<TResult> GetReadOnlyListAsync<TEntity, TResult>(
+        Func<IQueryable<TEntity>, IQueryable<TResult>> queryBuilder)
+        where TEntity : class;
+
+    Task AddAsync<TEntity>(TEntity item, CancellationToken cancellationToken = default) where TEntity : class;
+
+    void Remove<TEntity>(TEntity item) where TEntity : class;
+
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ GenericRepository/Repository.cs
@@ -0,0 +1,61 @@
+﻿using GenericRepository.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericRepository;
+
+public class Repository<TDbContext> : IRepository where TDbContext : DbContext
+{
+    private readonly TDbContext _context;
+
+    public Repository(TDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<TEntity?> GetAsync<TEntity>(
+        Func<IQueryable<TEntity>, IQueryable<TEntity>> queryBuilder,
+        CancellationToken cancellationToken = default)
+        where TEntity : class
+    {
+        return queryBuilder(_context.Set<TEntity>()).FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public Task<List<TEntity>> GetListAsync<TEntity>(
+        Func<IQueryable<TEntity>, IQueryable<TEntity>> queryBuilder,
+        CancellationToken cancellationToken = default)
+        where TEntity : class
+    {
+        return queryBuilder(_context.Set<TEntity>()).ToListAsync(cancellationToken);
+    }
+
+    public Task<TResult?> GetReadOnlyAsync<TEntity, TResult>(
+        Func<IQueryable<TEntity>, IQueryable<TResult>> queryBuilder,
+        CancellationToken cancellationToken = default)
+        where TEntity : class
+    {
+        return queryBuilder(_context.Set<TEntity>().AsNoTracking()).FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public IAsyncEnumerable<TResult> GetReadOnlyListAsync<TEntity, TResult>(
+        Func<IQueryable<TEntity>, IQueryable<TResult>> queryBuilder)
+        where TEntity : class
+    {
+        return queryBuilder(_context.Set<TEntity>().AsNoTracking()).AsAsyncEnumerable();
+    }
+
+    public async Task AddAsync<TEntity>(TEntity item, CancellationToken cancellationToken = default)
+        where TEntity : class
+    {
+        await _context.AddAsync(item, cancellationToken);
+    }
+
+    public void Remove<TEntity>(TEntity item) where TEntity : class
+    {
+        _context.Remove(item);
+    }
+
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return _context.SaveChangesAsync(cancellationToken);
+    }
+}
